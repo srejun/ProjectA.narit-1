2c552ad73db2fa5d7c7d3ee806a27dedac42a023
Unknown <bigboy2541@gmail.com>
2018-08-08 22:54:04 +0700

    getalldatasensor and demo db
    get all data sensor and demo db

--- App.js
+++ App.js
@@ -48,6 +48,42 @@ app.get('/api',function(req,res){
 })
 
 
+app.post('/api/getallsensor',function(req,res){
+    var date = new Date()
+    var mysort = { location: 1 };
+    
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("DataSensor");
+        dbo.collection("from").find({}).sort(mysort).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          console.log(result.length)
+          console.log(result[0].data.length)
+          console.log(result[1].data.length)
+          let max ={}
+          //console.log(JSON.stringify(result[0].data[0]))
+          for(var i=0;i<result.length;i++){
+              max[i]={'location':result[i].location,'inBuilding':result[i].inBuilding,'data':result[i].data[0]}
+              /* if(result[i].data[0].time.getTime()>max[i].data.time.getTime()){
+                console.log(max[i].data.time.getTime())
+                console.log(result[i].data[0].time.getTime())
+              } */
+              
+            for(var j=0;j<result[i].data.length;j++){
+                if(result[i].data[j].time.getTime()>max[i].data.time.getTime()){
+                    max[i].data=result[i].data[j]
+                }
+            }
+          }
+          console.log(max)
+          res.end(JSON.stringify(max))
+          db.close();
+        });
+    });
+    
+})
+
 
 app.post('/api',function(req,res){ //
     console.log('data input'+req.body)
@@ -139,7 +175,7 @@ app.post('/api/inputdata', function(req,res){ //sensor input update data
                 })
                 }
                 
-                //console.log("result from db"+JSON.stringify(result));
+                console.log("result from db"+JSON.stringify(result));
                 //console.log("11111"+hasplace) 
                 db.close();
                 
