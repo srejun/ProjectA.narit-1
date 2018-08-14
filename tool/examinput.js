bda4171ccae1656dbacd10dbea542f939fb10f11
Unknown <bigboy2541@gmail.com>
2018-08-14 22:41:02 +0700

    can input data more daily
    1.use path test
    2.use path testinput
    จบวันแล้วไปแก้บรรทัดที่ 21และ22 ของไฟล์ examinput และ test โดยบวกค่า day ไปทีละหนึ่ง
    - เพิ่มหรือลดช่วงเวลาของข้อมูลตรง timenow บรรทัดที่ 50 ของไฟล์ testinput

--- tool/examinput.js
+++ tool/examinput.js
@@ -18,13 +18,14 @@ exports.go = function (req, res) {
         var hour = date.getHours()
         var minute = date.getMinutes()
         var second = date.getSeconds()
-        var alltime = new Date(year + "/" + (month + 1) + "/" + day)
+        var alltime = new Date(year + "/" + (month + 1) + "/" + (day))
         var checktime = new Date(year + "/" + (month + 1) + "/" + (day + 1))
         if (req.body['data']['uv'] === undefined) { req.body['data']['uv'] = 0 }
         if (req.body['data']['wind'] === undefined) { req.body['data']['wind'] = 0 }
         if (req.body['data']['humidity'] === undefined) { req.body['data']['humidity'] = 0 }
         if (req.body['data']['temperature'] === undefined) { req.body['data']['temperature'] = 0 }
         let timenow
+        let datenow
         let newdata = {}
         newdata['location'] = req.body['location']
         newdata['inBuilding'] = req.body['inBuilding']
@@ -32,27 +33,28 @@ exports.go = function (req, res) {
         newdata['date'] = alltime.getTime()
         var adddata
         var find = "nodata"
-        dbo.collection("from").find({ location: req.body['location'], inBuilding: req.body['inBuilding'], date: newdata['date'] }).toArray(function (err, result) {
+        dbo.collection("from").find({ location: req.body['location'], inBuilding: req.body['inBuilding']}).toArray(function (err, result) {
             if (err) throw err;
             find = result.length
-            console.log(result.length)
-            console.log("res" + result[0].data.length)
-            timenow = result[0].data[result[0].data.length - 1].time
-            console.log("time" + timenow)
+            //console.log(result.length)
+            console.log("res" + result.length)
+            timenow = result[result.length - 1].data[result[result.length - 1].data.length - 1].time
+            datenow = result[result.length - 1].date
+            console.log("time" + datenow)
 
             if (result.length > 0) {
                 MongoClient.connect(url, function (err, db) {
                     if (err) throw err;
                     var dbo = db.db("DataSensor");
                     for (i = 0; i < 4000; i++) {
-                        timenow = timenow + (5000)
+                        timenow = timenow + (30000)
                         
                         req.body['data']['uv']=req.body['data']['uv']+1
                         req.body['data']['wind']=req.body['data']['wind']+1
                         req.body['data']['humidity']=req.body['data']['humidity']+1
                         req.body['data']['temperature']=req.body['data']['temperature']+1
                         newdata['data'] = [{ 'uv': req.body['data']['uv'], 'wind': req.body['data']['wind'], 'humidity': req.body['data']['humidity'], 'temperature': req.body['data']['temperature'], 'time': timenow }]
-                        console.log(newdata)
+                        //console.log(newdata)
                         // newdata['data']['time']=timenow
                         //console.log(newdata)
                         //console.log(result[0].date)
@@ -61,9 +63,9 @@ exports.go = function (req, res) {
                         adddata = { $push: { data: newdata.data[0] } }
                         if (timenow < checktime.getTime()) {
 
-                            dbo.collection("from").update({ location: req.body['location'], inBuilding: req.body['inBuilding'] }, adddata, function (err, res) {
+                            dbo.collection("from").update({ location: req.body['location'], inBuilding: req.body['inBuilding'],date: datenow }, adddata, function (err, res) {
                                 if (err) throw err;
-                                //console.log("1 document update");
+                                console.log("1 document update");
                                 db.close();
                             });
 
