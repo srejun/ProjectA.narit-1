cbeb733a7b50f25373b8aea4feae6b081b300f90
Unknown <supavitwork@gmail.com>
2018-08-14 11:08:03 +0700

    login conecect fornt complete
    -check session by id&email (col_ses)
    -check user by email(col_user)
    -add session sessionid email name isAdmin (col_ses)

--- tool/login.js
+++ tool/login.js
@@ -11,42 +11,35 @@ exports.go = function (req, res) {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db(DB);
-    /////////////check Session All///////////////////////
-    dbo.collection(col_ses).find({session_id:req.sessionID},function (err, result) {
-      if (result>0) {res.end(JSON.stringify(Bres))}
+    console.log(req.body)
+    /////////////check Session All(id&email)///////////////////////
+    dbo.collection(col_ses).find({session_id:String(req.sessionID),email:myobj.email}).toArray(function (err, result) {
+      
+      if (result.length>0) {res.end(JSON.stringify(Bres)),console.log("have session"+result)}
       else {
-            req.checkBody('email').isEmpty
-          req.checkBody('email').isEmail
-          req.checkBody('PASS', 'Password is Empty do it again').isEmpty
-          const errorValidate = req.validationErrors();
-          if (errorValidate) {console.log(JSON.stringify(errorValidate))} 
-          else {
             MongoClient.connect(url, function (err, db) {
               if (err) throw err;
               var dbo = db.db(DB);
               //////////////////have user/////////////////
               dbo.collection(col_user).find(myobj).toArray(function (err, result) {
-                  if(result.length<=0) {res.end(JSON.stringify(Bres))}
+                  if(result.length<=0) {res.end(JSON.stringify(Bres)),console.log("NO user pls regis")}
                   else {
-                    console.log("5555555555")
                     //////////compare password//////////////
                      myobj.PASS=req.body.PASS,myobj.isAdmin=result[0].isAdmin
-                    console.log(myobj.isAdmin)
                     bcrypt.compare(myobj.PASS,result[0].PASS, function(err, respone) {
                       if(respone)    { 
                          MongoClient.connect(url, function (err, db) {
                                       if (err) throw err;
-                                      Bres.name=result.name,Bres.isAdmin=result[0].isAdmin,Bres.comfirm=true  
+                                      Bres.name=result[0].name,Bres.isAdmin=result[0].isAdmin,Bres.comfirm=true  
                                       var dbo = db.db(DB);
-                                      var hour = 10000
+                                      var hour = 10*60*1000
                                       req.session.cookie.expires = new Date(Date.now() + hour)
                           req.session.cookie.maxAge = hour           
-                          req.session.user=result[0]
                                           /////////////add Session///////////////////////                                   
-                                      dbo.collection(col_ses).insertOne({session_id:req.sessionID,session:req.session,isAdmin:result[0].isAdmin},function (err, result) {
+                                      dbo.collection(col_ses).insertOne({"session_id":req.sessionID,isAdmin:result[0].isAdmin,name:result[0].name,email:result[0].email},function (err, result) {
                                         if (err) throw err;                                        
                                         res.end(JSON.stringify(Bres))
-                                        console.log('welcom user')
+                                        console.log('welcom user'+JSON.stringify(Bres)+req.sessionID)
                                       })
                                       db.close();
 
@@ -61,7 +54,7 @@ exports.go = function (req, res) {
               db.close();
             })
           }           
-      }
+      
     })
   db.close();
   })
