2812fdffb75aa7a383be870c4f3a69087cbf9890
Unknown <supavitwork@gmail.com>
2018-08-22 12:00:00 +0700

    show user

--- tool/showM.js
+++ tool/showM.js
@@ -4,27 +4,24 @@ exports.go=function(req,res){
     const col_ses="Sessions"
     const col_user="user"
     const DB="weather"
-    var Bres ={confirm: false,err:null,data=null}
+    var Bres ={confirm: false,err:null,data:null}
     var have_ses=null
     MongoClient.connect(url,async function (err, db) {
         if (err) throw err;
         var dbo = db.db(DB);
-       have_ses=await dbo.collection(col_ses).find({seesion_id:req.sessionID,isAdmin:true})
-          if (have_ses.length>0) {console.log('have user login'),Bres.err="same session"}
+        have_ses= await dbo.collection(col_ses).find({session_id:req.sessionID,isAdmin:true}).toArray() 
+          if (have_ses.length<=0) {console.log('permission denied'),Bres.err="not admin"}
           else{
-                dbo.collection(col_user).find({status:true}).toArray(function (err, result) {
-                if (err) throw err;
-                console.log("GET")
-                console.log(result);
-                Bres.data=result
-                res.end(Bres)
-                db.close();
-                });
+                var user= await dbo.collection(col_user).find({status:true}).project({_id : 0, name:1 , isAdmin:1, email:1}).toArray()
+                if(user.length<=0){Bres.err='No data'}
+                else{Bres.confirm=true,Bres.data=user}           
+                }
 
-                
-            }
+                console.log(JSON.stringify(have_ses[0]))
+                res.end(JSON.stringify(Bres))
+            
     
-    
+      db.close();
         })
 
 }
