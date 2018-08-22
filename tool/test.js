5278f7a38124c0920cd245096901fc370e171bc7
Unknown <bigboy2541@gmail.com>
2018-08-22 23:36:56 +0700

    แก้บัคที่ location ซ้ำ

--- tool/test.js
+++ tool/test.js
@@ -36,6 +36,11 @@ exports.go = function (req, res) {
         //console.log(req.body['data']['humidity'])
         if (req.body['data']['temperature'] === undefined) { req.body['data']['temperature'] = 0 }
         //console.log(req.body['data']['temperature'])
+
+        var aveuv = null
+        var avewind = null
+        var avehumidity = null
+        var avetem = null
         let newdata = {}
         newdata['inBuilding'] = req.body['inBuilding']
         newdata['rate'] = 5
@@ -44,24 +49,25 @@ exports.go = function (req, res) {
 
         var datalo = {}
         var newlo = {}
-        datalo = {location:req.body['location']}
+        newlo['location']=req.body['location']
+        datalo = { location: req.body['location'] }
         var haslo = true
-        const find = await dbo.collection("location").find().toArray()     //insert new location
+        const find = await dbo.collection("location").find({datalo:newlo}).toArray()     //insert new location
         //console.log("lo"+JSON.stringify(find[0].location))
         //console.log(data.location)
         //console.log(typeof(find.length))
         //console.log(find.hasOwnProperty(0))
         //console.log(find.length)
-        if (find.hasOwnProperty(0)) {
+        if (find.hasOwnProperty(0) === true) {
             for (var i = 0; i < find.length; i++) {
-                console.log("infile"+find[0].datalo.location[i])
-                if (find[0].datalo.location[i] === datalo.location) {  
+                console.log("infile" + find[0].datalo.location[i])
+                if (find[0].datalo.location[i] === datalo.location) {
                     haslo = false
                 }
             }
-            
+
             if (haslo) {
-                dbo.collection("location").insertOne({datalo}, function (err, res) {
+                dbo.collection("location").insertOne({ datalo }, function (err, res) {
                     if (err) throw err;
                     console.log("1 document update");
                     db.close();
@@ -69,7 +75,7 @@ exports.go = function (req, res) {
             }
         }
         else {
-            dbo.collection("location").insertOne({datalo}, function (err, res) {
+            dbo.collection("location").insertOne({ datalo }, function (err, res) {
                 if (err) throw err;
                 console.log("1 document insert");
                 db.close();
@@ -85,11 +91,15 @@ exports.go = function (req, res) {
         } */
 
         if (result.length > 0 && result[0].date < checktime.getTime()) {
-
+            aveuv = ((result[0].data.length * result[0].ave[0].uv) + newdata.data[0].uv) / (result[0].data.length + 1)
+            avewind = ((result[0].data.length * result[0].ave[0].wind) + newdata.data[0].wind) / (result[0].data.length + 1)
+            avehumidity = ((result[0].data.length * result[0].ave[0].humidity) + newdata.data[0].humidity) / (result[0].data.length + 1)
+            avetem = ((result[0].data.length * result[0].ave[0].temperature) + newdata.data[0].temperature) / (result[0].data.length + 1)
+            newdata['ave'] = [{ 'uv': aveuv, 'wind': avewind, 'humidity': avehumidity, 'temperature': avetem }]
             MongoClient.connect(url, function (err, db) {
                 if (err) throw err;
                 var dbo = db.db("DataSensor");
-                var adddata = { $push: { data: newdata.data[0] } }
+                var adddata = { $push: { data: newdata.data[0] }, $set: { ave: newdata.ave } }
                 dbo.collection(req.body['location']).update({ inBuilding: req.body['inBuilding'] }, adddata, function (err, res) {
                     if (err) throw err;
                     console.log("1 document update");
@@ -98,6 +108,7 @@ exports.go = function (req, res) {
             })
         }
         else {
+            newdata['ave'] = [{ 'uv': req.body['data']['uv'], 'wind': req.body['data']['wind'], 'humidity': req.body['data']['humidity'], 'temperature': req.body['data']['temperature'] }]
             MongoClient.connect(url, function (err, db) {
                 if (err) throw err;
                 var dbo = db.db("DataSensor");
