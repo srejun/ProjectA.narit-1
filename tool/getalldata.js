48374c9313f41a6b561e706106d8fbc1ba550d17
Unknown <bigboy2541@gmail.com>
2018-08-23 11:49:48 +0700

    เพิ่ม check session
    เพิ่ม check session plot graph และ getalldata
    แก้ไข file inputdata ตรง add ข้อมูลผิด(ข้างในข้างนอก)

--- tool/getalldata.js
+++ tool/getalldata.js
@@ -4,11 +4,13 @@ exports.go = function (req, res) {
     var date = new Date()
     var mysort = { date: 1 };
     var max = {}
+    var value = {}
     var maxin = {}
     var maxout = {}
     MongoClient.connect(url, async function (err, db) {
         if (err) throw err;
         var dbo = db.db("DataSensor");
+        var have_ses= await dbo.collection(col_ses).find({session_id:String(req.sessionID)}).toArray() 
         var findalllocation = await dbo.collection("location").find({}).sort(mysort).toArray()
         console.log(findalllocation.length)
         for (var l = 0; l < findalllocation.length; l++) {
@@ -17,18 +19,36 @@ exports.go = function (req, res) {
             var eachlocationout = await dbo.collection(findalllocation[l].datalo.location).find({ inBuilding: false }).sort(mysort).toArray()
             console.log(eachlocationin.length)
             console.log(eachlocationout.length)
+            
             //await console.log(eachlocationin[eachlocationin.length-1].data.length)
             //await console.log(eachlocationin[eachlocationin.length-1].data[eachlocationin[eachlocationin.length-1].data.length-1])
             //await console.log(eachlocationout[eachlocationout.length-1].data.length)
             //await console.log(eachlocationout[eachlocationout.length-1].data[eachlocationout[eachlocationout.length-1].data.length-1])
             if (eachlocationin.length > 0) {
-                maxin[l] = { 'inBuilding': true, 'data': eachlocationin[eachlocationin.length - 1].data[eachlocationin[eachlocationin.length - 1].data.length - 1] }
+                if(eachlocationin[eachlocationin.length - 1].data[eachlocationin[eachlocationin.length - 1].data.length - 1].humidity>40){
+                    maxin[l] = { 'inBuilding': true, 'data': eachlocationin[eachlocationin.length - 1].data[eachlocationin[eachlocationin.length - 1].data.length - 1],'flag':0 }
+                }
+                else if(eachlocationin[eachlocationin.length - 1].data[eachlocationin[eachlocationin.length - 1].data.length - 1].humidity>50){
+                    maxin[l] = { 'inBuilding': true, 'data': eachlocationin[eachlocationin.length - 1].data[eachlocationin[eachlocationin.length - 1].data.length - 1],'flag':1 }
+                }
+                else{
+                    maxin[l] = { 'inBuilding': true, 'data': eachlocationin[eachlocationin.length - 1].data[eachlocationin[eachlocationin.length - 1].data.length - 1],'flag':2 }
+                }
+                
             }
             else {
                 maxin[l] = { 'inBuilding': true, 'data': null }
             }
             if (eachlocationout.length > 0) {
-                maxout[l] = { 'inBuilding': false, 'data': eachlocationout[eachlocationout.length - 1].data[eachlocationout[eachlocationout.length - 1].data.length - 1] }
+                if(eachlocationout[eachlocationout.length - 1].data[eachlocationout[eachlocationout.length - 1].data.length - 1].humidity>40){
+                    maxout[l] = { 'inBuilding': false, 'data': eachlocationout[eachlocationout.length - 1].data[eachlocationout[eachlocationout.length - 1].data.length - 1],'flag':0 }
+                }
+                else if(eachlocationout[eachlocationout.length - 1].data[eachlocationout[eachlocationout.length - 1].data.length - 1].humidity>50){
+                    maxout[l] = { 'inBuilding': false, 'data': eachlocationout[eachlocationout.length - 1].data[eachlocationout[eachlocationout.length - 1].data.length - 1],'flag':1 }
+                }
+                else{
+                    maxout[l] = { 'inBuilding': false, 'data': eachlocationout[eachlocationout.length - 1].data[eachlocationout[eachlocationout.length - 1].data.length - 1],'flag':2 }
+                }
             }
             else {
                 maxout[l] = { 'inBuilding': false, 'data': null }
@@ -43,7 +63,7 @@ exports.go = function (req, res) {
             // }
 
         }
-        console.log(JSON.stringify(max))
+        //console.log(JSON.stringify(max))
         //console.log("innnnn" + JSON.stringify(maxin))
         //console.log("outttt" + JSON.stringify(maxout))
         /* console.log(result);
@@ -68,7 +88,16 @@ exports.go = function (req, res) {
         //     }
         // }
         // console.log(max)
-        res.end(JSON.stringify(max))
+        if(have_ses.length>0){
+            value={confirm:true,data:max}
+            res.end(JSON.stringify(value))
+        }
+        else{
+            value={confirm:false,data:null}
+            res.end(JSON.stringify(value))
+        }
+        //value={confirm:true,data:max}
+        //res.end(JSON.stringify(value))
         db.close();
 
     });
