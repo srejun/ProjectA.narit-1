5d8f994148f21ccdf986d15db7921751175f805a
Denice <ton11797@hotmail.com>
2018-08-30 14:40:08 +0000

    auto deploy and deploy with debug

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git pull
+sudo docker stop backend
+sudo docker container rm backend
+sudo docker build -t denice/nodejsapp .
+sudo docker run -d -p 1111:8081 --name backend denice/nodejsapp
